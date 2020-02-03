333efbd17b705f5cec473a42f13a9f1d67ddb99d
nazarperkatyuk <perkatyuk.n@gmail.com>
2020-02-03 18:20:51 +0200

    remmuving

--- src/task1/script.js
+++ src/task1/script.js
@@ -1,13 +1,23 @@
-//function step(a,b){
-//    return a ** b;
+//function sum(arr){
+//    let res = arr.reduce((previousValue, currentValue) => (previousValue + currentValue));
+//    console.log(res);
 //}
-//console.log(step(8,-4);
+//console.log(sum([2,3,5,10]));
 
-function checkAge(year){
-    if (year >= 18)
-        alert("Доступ дозволено");
+
+//function addArrAfterPos(arr, arrToPaste, position){
+//    for(let i = 0; i < arrToPaste.length; i++){
+//         arr.splice(position+i,0,arrToPaste[i]);
+//    }
+//console.log(arr);
+//}
+//addArrAfterPos([2, 3, 4, 5, 10, 11, 12, 13], [6, 7, 8, 9], 4);
+
+function getPosOfArr(arr, num){
+    let position = arr.indexOf(num);
+    if(position == -1)
+        console.log('Немає такого елементу');
     else
-        alert("У доступі відмовлено");
+        console.log(position);
 }
-let year = +prompt("Enter your age");
-checkAge(year);
+    getPosOfArr([3, 92, 2, 8, 4, 1], 5);
